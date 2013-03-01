41834577e86fc1d70e6d2af22da94ae8d16aa145
uuf <uceumern@gmail.com>
2013-03-01 17:42:25 +0100

    decreased timeout

--- workspace/tv-show-crawler/src/com/example/tvshowcrawler/TVShow.java
+++ workspace/tv-show-crawler/src/com/example/tvshowcrawler/TVShow.java
@@ -731,7 +731,7 @@ public class TVShow implements JSONable, Parcelable
 		{
 			HttpURLConnection huc = (HttpURLConnection) url.openConnection();
 			HttpURLConnection.setFollowRedirects(true);
-			huc.setConnectTimeout(5 * 1000);
+			huc.setConnectTimeout(3 * 1000);
 			huc.setRequestMethod("GET");
 			huc.setRequestProperty("User-Agent",
 					"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
