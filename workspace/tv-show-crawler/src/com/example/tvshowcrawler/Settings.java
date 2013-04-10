2e0f695806fd065137fe245ef16ac62e2bf100df
uuf <uceumern@gmail.com>
2013-04-10 15:45:44 +0200

    removed enable transmission setting
    * non-transmission mode is no longer supported

--- workspace/tv-show-crawler/src/com/example/tvshowcrawler/Settings.java
+++ workspace/tv-show-crawler/src/com/example/tvshowcrawler/Settings.java
@@ -21,30 +21,17 @@ public class Settings
 		return context;
 	}
 
-	public boolean getEnableTransmission()
-	{
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		return prefs.getBoolean(context.getString(R.string.pref_key_enable_transmission), false);
-	}
-
 	public boolean getPrefer720p()
 	{
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		return prefs.getBoolean(context.getString(R.string.pref_key_720p), false);
 	}
 
-//	public void setCatchUp(boolean newValue)
-//	{
-//		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-//		Editor prefsEditor = prefs.edit();
-//		prefsEditor.putBoolean(context.getString(R.string.pref_key_catchup), newValue);
-//		prefsEditor.commit();
-//	}
-
 	public String getTransmissionServer()
 	{
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		return prefs.getString(context.getString(R.string.pref_key_transmission_server), "");
+		return prefs.getString(context.getString(R.string.pref_key_transmission_server),
+				context.getString(R.string.pref_transmission_server_default));
 	}
 
 	public boolean getX264()
